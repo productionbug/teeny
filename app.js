64de388604f7cc16015340d5d838802ea4e0dc33
nhowlade <nhowlade@uoguelph.ca>
2020-07-13 20:43:45 -0400

    Used body-parser and created a handler for default route

--- app.js
+++ app.js
@@ -7,7 +7,12 @@ const port = process.env.PORT || 3000;
 const app = express();
 
 app.use(morgan("tiny"));
+app.use(bodyParser.json());
+
+app.get("/", (req, res, next) => {
+  res.send("Hello World");
+});
 
 app.listen(port, () => {
-	console.log(`Server is listening at port: ${port}`);
+  console.log(`Server is listening at port: ${port}`);
 });
