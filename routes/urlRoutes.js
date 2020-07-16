3b5e4d7481dcd0caf86958f7d38a7b18bef745e6
nhowlade <nhowlade@uoguelph.ca>
2020-07-16 19:00:44 -0400

    populated urlRoutes

---
+++ routes/urlRoutes.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const router = express.Router();
+
+router.get("/", () => {});
+
+router.get("/teeny/:shorturl", () => {});
+
+router.post("/teeny", () => {});
+
+module.exports = router;
