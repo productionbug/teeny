62746b4effc69e2044de55ca914a2516edc06247
nhowlade <nhowlade@uoguelph.ca>
2020-07-29 21:34:19 -0400

    putting in nanoid

--- controller/url.controller.js
+++ controller/url.controller.js
@@ -1,13 +1,18 @@
+const { customAlphabet } = require("nanoid");
+
 exports.getHomepage = (req, res, next) => {
-	res.json({ message: "inside homepage" });
+  res.json({ message: "inside homepage" });
 };
 
 exports.postTeeny = (req, res, next) => {
-	console.log(req.body.teeny);
-	res.send("inside post teeny");
+  const nanoid = customAlphabet(/^[A-Za-z0-9]+$/, 9);
+  const shortUrl = nanoid();
+  console.log("Here is the shortURL: ", shortUrl);
+  //   console.log(req.body.teeny);
+  res.send("inside post teeny");
 };
 
 exports.getLongUrl = (req, res, next) => {
-	console.log(req.params.shorturl);
-	res.json({ message: "inside longurl" });
+  console.log(req.params.shorturl);
+  res.json({ message: "inside longurl" });
 };
