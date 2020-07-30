7fb5a62b65f6306cc74b9bda472171d902f6cb73
niloyKGhosh <niloykumar256@gmail.com>
2020-07-29 21:11:24 -0400

    Installed nano id

--- model/url.model.js
+++ model/url.model.js
@@ -3,18 +3,18 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const urlSchema = new Schema(
-  {
-    longUrl: {
-      type: String,
-      required: true,
-    },
-    shortUrl: {
-      type: String,
-      required: true,
-      unique: true,
-    },
-  },
-  { timestamps: true }
+	{
+		longUrl: {
+			type: String,
+			required: true,
+		},
+		shortUrl: {
+			type: String,
+			required: true,
+			unique: true,
+		},
+	},
+	{ timestamps: true }
 );
 
 const Url = mongoose.model("url", urlSchema);
