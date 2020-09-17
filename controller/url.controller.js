fe35b8d4049e9379cc6a9a72cfcecfdb75dc8cce
nhowlade <nhowlade@uoguelph.ca>
2020-09-16 23:34:35 -0400

    Tried to add 404 page

--- controller/url.controller.js
+++ controller/url.controller.js
@@ -4,56 +4,56 @@ const validator = require("validator");
 const Url = require("../model/url.model");
 
 exports.getHomepage = (req, res, next) => {
-	res.render("index.ejs", {
-		// url: { teeny: "undefined" },
-		url: { teeny: "teeny.cc" },
-	});
+    res.render("index.ejs", {
+        // url: { teeny: "undefined" },
+        url: { teeny: "teeny.cc" },
+    });
 };
 
 exports.postTeeny = async (req, res, next) => {
-	const longUrl = req.body.url;
+    const longUrl = req.body.url;
 
-	try {
-		if (!longUrl) {
-			throw new Error("Url field cannot be empty");
-		} else if (!validator.isURL(longUrl)) {
-			throw new Error("Please send in a valid url");
-		}
+    try {
+        if (!longUrl) {
+            throw new Error("Url field cannot be empty");
+        } else if (!validator.isURL(longUrl)) {
+            throw new Error("Please send in a valid url");
+        }
 
-		const nanoid = customAlphabet(
-			"1234567890abcdefghigklmnopqrstuvwxyz",
-			9
-		);
+        const nanoid = customAlphabet(
+            "1234567890abcdefghigklmnopqrstuvwxyz",
+            9
+        );
 
-		// Creating a new Url Schema and saving it to the database
-		const shortUrl = nanoid();
-		const url = await Url.create({ longUrl, shortUrl });
-		const result = {
-			// teeny: `https://teeny.cc/${url.toObject().shortUrl}`,
-			teeny: `http://localhost:3000/${url.toObject().shortUrl}`,
-		};
-		res.status(201);
+        // Creating a new Url Schema and saving it to the database
+        const shortUrl = nanoid();
+        const url = await Url.create({ longUrl, shortUrl });
+        const result = {
+            // teeny: `https://teeny.cc/${url.toObject().shortUrl}`,
+            teeny: `http://localhost:3000/${url.toObject().shortUrl}`,
+        };
+        res.status(201);
 
-		res.render("index.ejs", { url: result });
-	} catch (e) {
-		next(e);
-	}
+        res.render("index.ejs", { url: result });
+    } catch (e) {
+        next(e);
+    }
 };
 
 exports.getLongUrl = async (req, res, next) => {
-	const shortUrl = req.params.shorturl;
-	try {
-		const url = await Url.findOne({ shortUrl }).lean().exec();
-		if (url === null) {
-			//Call 404 here
-			// return res.status(404).send("URL not found");
-			return next();
-		}
-		// Redirect here
-		return res.send(url.longUrl);
-	} catch (e) {
-		return next(e);
-	}
+    const shortUrl = req.params.shorturl;
+    try {
+        const url = await Url.findOne({ shortUrl }).lean().exec();
+        if (url === null) {
+            //Call 404 here
+            // return res.status(404).send("URL not found");
+            return next();
+        }
+        // Redirect here
+        return res.send(url.longUrl);
+    } catch (e) {
+        return next(e);
+    }
 };
 
 // TODO: Call res.redirect()
